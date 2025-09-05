1e7c91e54d6974280d24a9741ecba9098eaefb77
nafas-gregoriusaldi <gregorius.bagaskoro@nafas.co.id>
2025-09-05 14:42:40 +0700

    :zap

--- app/Models/TpsActivity.php
+++ app/Models/TpsActivity.php
@@ -10,9 +10,13 @@ class TpsActivity extends Model
     protected $fillable = ['container_no_plat', 'masuk', 'keluar', 'foto_masuk_depan','foto_masuk_belakang','foto_masuk_kanan','foto_masuk_kiri',
         'foto_keluar_depan','foto_keluar_belakang','foto_keluar_kanan','foto_keluar_kiri'];
 
-    public function container()
-    {
-        return $this->belongsTo(Container::class);
-    }
+    // public function container()
+    // {
+    //     return $this->belongsTo(Container::class);
+    // }
+    public function containers()
+        {
+            return $this->hasMany(Container::class, 'no_plat', 'no_plat');
+        }
     
 }
