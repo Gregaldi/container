fbbe85fd9210c7ebf15cbd1bb2fc52ad82b02a5d
nafas-gregoriusaldi <gregorius.bagaskoro@nafas.co.id>
2025-09-05 13:37:24 +0700

    :zap:

--- app/Models/Container.php
+++ app/Models/Container.php
@@ -11,13 +11,25 @@ class Container extends Model
     protected $keyType = 'string';
     protected $fillable = ['nomor_container', 'size', 'asal', 'no_plat', 'no_seal', 'foto_no_plat','foto_no_seal', 'foto_nomor_container'];
 
-    public function terminalActivities()
-    {
-        return $this->hasMany(TerminalActivity::class);
-    }
+    // public function terminalActivities()
+    // {
+    //     return $this->hasMany(TerminalActivity::class);
+    // }
 
-    public function tpsActivities()
-    {
-        return $this->hasMany(TpsActivity::class);
-    }
+    // public function tpsActivities()
+    // {
+    //     return $this->hasMany(TpsActivity::class);
+    // }
+
+    public function terminalActivities()
+{
+    // Assuming the foreign key in terminal_activities is 'nomor_container'
+    return $this->hasMany(TerminalActivity::class, 'nomor_container', 'nomor_container');
+}
+
+public function tpsActivities()
+{
+    // Assuming the foreign key in tps_activities is 'nomor_container'
+    return $this->hasMany(TpsActivity::class, 'nomor_container', 'nomor_container');
+}
 }
