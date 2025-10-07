f1c44d463230ac941b1f59e7c2fcbf2fa37c40be
nafas-gregoriusaldi <gregorius.bagaskoro@nafas.co.id>
2025-10-08 00:24:23 +0700

    xx

--- app/Models/ContainerMovements.php
+++ app/Models/ContainerMovements.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ContainerMovements extends Model
 {
     //
-    protected $fillable = ['container_id','direction','truck_plate','truck_plate_out','seal_ship','seal_tps','photos','photo_out','notes','timestamp'];
+    protected $fillable = ['container_id','direction','truck_plate','truck_plate_out','seal_ship','seal_tps','photos','photos_out','notes','timestamp'];
 
 
     protected $casts = [
