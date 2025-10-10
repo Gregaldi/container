67d1b9c1ded531eac11bba8451cd18423a626dcb
nafas-gregoriusaldi <gregorius.bagaskoro@nafas.co.id>
2025-10-10 13:52:22 +0700

    api

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::get('/ping', function () {
 Route::apiResource('containers', ContainerController::class);
 // Route::get('/containers/{container_number}', [ContainerController::class, 'show']);
 Route::get('/container-movements', [MovementController::class, 'index']);
-Route::get('/container-find', [MovementController::class, 'findContainerIn']);
+Route::get('/container-find/{container_number}', [MovementController::class, 'findContainerIn']);
 Route::post('/movements/in', [MovementController::class, 'storeIn']);
 Route::post('/movements/out', [MovementController::class, 'storeOut']);
 Route::get('/movements/{container_number}', [MovementController::class, 'detailindex']);
